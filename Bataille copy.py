901c900153a4af80d31b6fd66b5690ef711b9428
jkaeuffer <josephinegavignet@gmail.com>
2015-07-21 12:57:05 -0700

    creating the game of bataille file

---
+++ Bataille copy.py
@@ -0,0 +1,13 @@
+#the game of bataille takes as many turns as there as carde left in the dark. turn by turn, players will uncover a card from their randomly organized share of the deck, the highest card wins. If both cards are of the same values then nobody wins and they go another round.
+#at the end of the turn, the winner takes all cards that were shown and places them at the bottom of their pile of cards.
+#game is over when one player has all the cards
+
+# Steps to follow:
+# 1. create a list that has all the cards in a deck
+# 2. create two players deck and assign in a random order half of the deck into their list
+# 3 for each turn, take the first card in the list for that player, remove that card from the list and show it. then do the same for the other player
+# 4 then compare the two cards and decide if they're equal or not. if they're not equal then look at which is the winner.
+# 5 take both cards that were shown and add them to the end of the winner's' card list
+# 6 if they are equal then have them go another round (to simplify we could use color suits as a tie break rule)
+# 7 check if one users list is empty, if it is, this player has lost.
+
