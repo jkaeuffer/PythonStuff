8189092d96a360af5b073d837d5b2a1ee0d01c57
jkaeuffer <josephinegavignet@gmail.com>
2015-07-24 09:40:51 -0700

    deal function

--- Bataille copy.py
+++ Bataille copy.py
@@ -11,3 +11,33 @@
 # 6 if they are equal then have them go another round (to simplify we could use color suits as a tie break rule)
 # 7 check if one users list is empty, if it is, this player has lost.
 
+import random
+
+deckOfCards = ['Ac', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ad', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'Jd', 'Qd', 'Kd', 'As', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks', 'Ah', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'Jh', 'Qh', 'Kh']
+
+#shuffle and deal cards
+
+def dealCards(cards):
+	deckToShuffle = random.shuffle(cards)
+	playerA = []
+	playerB = []
+	for n in range(len(cards) - 1):
+		if n % 2 != 0:
+			playerA.append(cards[n])
+		else:
+			playerB.append(cards[n])
+	return playerA, playerB
+
+(playerACards, playerBCards) = dealCards(deckOfCards)
+
+cards = {
+	"Player A:": playerACards,
+	"Player B:": playerBCards
+}
+
+print cards
+
+
+
+
+
