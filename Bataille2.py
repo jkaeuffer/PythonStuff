13184d889b4403c6342e2ec605e2b98391a021d3
jkaeuffer <josephinegavignet@gmail.com>
2015-07-26 18:04:26 -0700

    new functionality
    - created a function to create deck of cards
    - created function to find the index of the card’s suit, so that we can
    map it to its value in the deck’s dict
    - started function to find the winning card

--- Bataille2.py
+++ Bataille2.py
@@ -18,46 +18,64 @@ import random
 # deckOfCards = ['Ac', '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ad', '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'Jd', 'Qd', 'Kd', 'As', '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks', 'Ah', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'Jh', 'Qh', 'Kh']
 
 deckVersion2 = {
-	"Ranks": '2345678910JQKA',
+	"Ranks": ["2","3","4","5","6","7","8","9","10",'J','Q','K','A'],
 	"Suits": 'hsdc', 
-	"Values": [14,2,3,4,5,6,7,8,9,10,11,12,13]
+	"Values": [2,3,4,5,6,7,8,9,10,11,12,13, 14]
 }
 
 def createDeck():
+	suitsList = [n for n in deckVersion2['Suits']]
 	deckOfCards = []
-	for n in range(len(deckVersion2["Ranks"])-1):
-		
+	for n in deckVersion2["Ranks"]:
+		for x in range (len(suitsList)-1):
+			deckOfCards.append(n + suitsList[x])
+	return deckOfCards
 
-print deckOfCards
+deckOfCards = createDeck()
 
 
-#shuffle and deal cards
+# shuffle and deal cards
 
-# def dealCards(cards):
-# 	deckToShuffle = random.shuffle(cards)
-# 	playerA = []
-# 	playerB = []
-# 	for n in range(len(cards) - 1):
-# 		if n % 2 != 0:
-# 			playerA.append(cards[n])
-# 		else:
-# 			playerB.append(cards[n])
-# 	return playerA, playerB
+def dealCards(cards):
+	deckToShuffle = random.shuffle(cards)
+	playerA = []
+	playerB = []
+	for n in range(len(cards) - 1):
+		if n % 2 != 0:
+			playerA.append(cards[n])
+		else:
+			playerB.append(cards[n])
+	return playerA, playerB
 
-# (playerACards, playerBCards) = dealCards(deckOfCards)
+(playerACards, playerBCards) = dealCards(deckOfCards)
 
-# # pick top card for each player
+# pick top card for each player
 
-# def pickCards(x, y):
-# 	playerAPick = x.pop()
-# 	playerBPick = y.pop()
-# 	return playerAPick, playerBPick
+def pickCards(x, y):
+	playerAPick = x.pop()
+	playerBPick = y.pop()
+	return playerAPick, playerBPick
 
-# (playerAPick, playerBPick) = pickCards(playerACards, playerBCards)
+(playerAPick, playerBPick) = pickCards(playerACards, playerBCards)
 
-# # check best card
+# print deckVersion2["Ranks"].index("J")
 
-# # def bestCard(x, y):
+# find index in dictionary from a rank
+def indexInDict(a):
+	rankIndex = deckVersion2["Ranks"].index(a)
+	return rankIndex
+
+# print deckVersion2["Values"][indexInDict(playerAPick[:-1])]
+
+# check best card
+
+# def bestCard(x, y):
+# 	cardRankPlayerA = x[:-1]
+# 	cardRankPlayerB = y[:-1]
+# 	cardValuePlayerA = deckVersion2["Values"][indexInDict(cardRankPlayerA)]
+
+
+
 
 
 
