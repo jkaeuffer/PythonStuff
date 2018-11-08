485e9ead68a09075b86e1714408df90d5e1ebd27
jkaeuffer <jgavignet@change.org>
2018-11-07 23:08:33 -0800

    Optimize by removing swap helper function

--- algos/sort/bubble.py
+++ algos/sort/bubble.py
@@ -1,19 +1,12 @@
 # Bubble Sort Implementation
 
 
-# Helper function for swaps
-def swap(array, idx_1, idx_2):
-    temp = array[idx_1]
-    array[idx_1] = array[idx_2]
-    array[idx_2] = temp
-
-
 def bubble_sort(my_list):
     rounds = len(my_list) - 1
     for i in my_list:
         for n in range(rounds):
             if my_list[n] > my_list[n + 1]:
-                swap(my_list, n, n + 1)
+                my_list[n], my_list[n + 1] = my_list[n + 1], my_list[n]
 
 
 lst1 = [0, 2, 4, 1, -5, 45]
